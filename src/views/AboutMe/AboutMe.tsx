23cf8f37cb19df0d5c5f014cfcc7c844665f8ea0
jason-ptm <js.solarte29@gmail.com>
2024-01-31 16:39:43 -0500

    feat: download cv button, no logic yet

--- src/views/AboutMe/AboutMe.tsx
+++ src/views/AboutMe/AboutMe.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
-import './style/index.css';
+import CloudDownloadOutlinedIcon from '@mui/icons-material/CloudDownloadOutlined';
 import { Box, Typography, useTheme } from '@mui/material';
-import { ContentLimitator } from '../../components';
-import AboutImage from '../../assets/about-me.png';
+import React, { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
+import AboutImage from '../../assets/about-me.png';
+import { ContentLimitator } from '../../components';
 import colors from '../../utils/constants/colors.json';
+import './style/index.css';
 
 interface IAboutMeProps {}
+
+interface IButtonsProps {
+  label: string;
+  icon: ReactNode;
+}
+
+const Button: React.FC<IButtonsProps> = ({ label, icon }) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        cursor: 'pointer',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '250px',
+        height: '50px',
+        borderRadius: '4px',
+        border: `1.5px solid ${colors[theme.palette.mode].black[400]}`,
+        gap: '8px',
+        boxShadow: `8px 8px 0px 0px ${colors[theme.palette.mode].blue[500]}`,
+        transition: 'all 0.8s',
+        '&:hover': {
+          backgroundColor: `${colors[theme.palette.mode].blue[1000]}68`,
+        },
+      }}
+    >
+      <Typography align="center" fontFamily="Inter" fontWeight={300}>
+        {label}
+      </Typography>
+      {icon}
+    </Box>
+  );
+};
 
 const AboutMe: React.FC<IAboutMeProps> = () => {
   const { t } = useTranslation();
   const theme = useTheme();
+
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
       <ContentLimitator>
@@ -34,16 +72,17 @@ const AboutMe: React.FC<IAboutMeProps> = () => {
               height: '400px',
               objectFit: 'cover',
               flexGrow: 1,
-              borderRadius: '15px',
+              borderRadius: '8px',
               boxShadow: '0px 0px 50px 0px rgba(158,155,158,0.3)',
             }}
           />
           <Box
             sx={{
-              flexGrow: 1,
-              flexBasis: '280px',
+              flexGrow: 1.5,
+              flexBasis: '300px',
               display: 'flex',
               flexDirection: 'column',
+              alignItems: 'center',
               gap: '25px',
             }}
           >
@@ -61,6 +100,10 @@ const AboutMe: React.FC<IAboutMeProps> = () => {
             <Typography variant="h6" fontWeight={300}>
               {t('about.text2')}
             </Typography>
+            <Button
+              label={t('about.cv')}
+              icon={<CloudDownloadOutlinedIcon />}
+            />
           </Box>
         </Box>
       </ContentLimitator>
