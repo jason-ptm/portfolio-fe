8f90ad96356db2e657ad6e7bb24e3f6c99cdce65
jason-ptm <js.solarte29@gmail.com>
2024-02-10 13:28:12 -0500

    feat: match dark mode

--- src/views/Footer/Footer.tsx
+++ src/views/Footer/Footer.tsx
@@ -7,6 +7,7 @@ import colors from '../../utils/constants/colors.json';
 import './style/index.css';
 import config from '../../utils/constants/configs.json';
 import CopyToClipboardButton from '../../components/CopyToClipBoard/CopyToClipBoard';
+import { darkModeKey } from '../../utils';
 
 interface IFooterProps {}
 
@@ -22,7 +23,10 @@ const Footer: React.FC<IFooterProps> = () => {
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        backgroundColor: colors[theme.palette.mode].black[1000],
+        backgroundColor:
+          theme.palette.mode === darkModeKey
+            ? colors[theme.palette.mode].black[200]
+            : colors[theme.palette.mode].black[1000],
         color: colors[theme.palette.mode].black[100],
         padding: '80px 0',
         justifyContent: 'center',
@@ -45,7 +49,10 @@ const Footer: React.FC<IFooterProps> = () => {
             justifyContent: 'center',
             alignItems: 'center',
             gap: '30px',
-            backgroundColor: colors[theme.palette.mode].black[1000],
+            backgroundColor:
+              theme.palette.mode === darkModeKey
+                ? colors[theme.palette.mode].black[200]
+                : colors[theme.palette.mode].black[1000],
             padding: '0 30px',
             position: 'relative',
             zIndex: 2000,
@@ -53,40 +60,58 @@ const Footer: React.FC<IFooterProps> = () => {
           }}
         >
           <InstagramIcon
-            fontSize="large"
             sx={{
+              color:
+                theme.palette.mode === darkModeKey
+                  ? colors[theme.palette.mode].black[1000]
+                  : colors[theme.palette.mode].black[100],
               width: '60px',
               height: '60px',
               cursor: 'pointer',
               padding: '10px',
               '&:hover': {
-                color: colors[theme.palette.mode].red[500],
+                color:
+                  theme.palette.mode === darkModeKey
+                    ? colors[theme.palette.mode].red[100]
+                    : colors[theme.palette.mode].red[500],
               },
             }}
             onClick={() => openInNewTab(config.links.instagram)}
           />
-
           <LinkedInIcon
             sx={{
+              color:
+                theme.palette.mode === darkModeKey
+                  ? colors[theme.palette.mode].black[1000]
+                  : colors[theme.palette.mode].black[100],
               width: '60px',
               height: '60px',
               cursor: 'pointer',
               padding: '10px',
               '&:hover': {
-                color: colors[theme.palette.mode].blue[700],
+                color:
+                  theme.palette.mode === darkModeKey
+                    ? colors[theme.palette.mode].blue[400]
+                    : colors[theme.palette.mode].blue[700],
               },
             }}
             onClick={() => openInNewTab(config.links.linkedin)}
           />
           <GitHubIcon
-            fontSize="large"
             sx={{
+              color:
+                theme.palette.mode === darkModeKey
+                  ? colors[theme.palette.mode].black[1000]
+                  : colors[theme.palette.mode].black[100],
               width: '60px',
               height: '60px',
               cursor: 'pointer',
               padding: '10px',
               '&:hover': {
-                color: colors[theme.palette.mode].blue[500],
+                color:
+                  theme.palette.mode === darkModeKey
+                    ? colors[theme.palette.mode].blue[800]
+                    : colors[theme.palette.mode].blue[500],
               },
             }}
             onClick={() => openInNewTab(config.links.github)}
@@ -114,21 +139,36 @@ const Footer: React.FC<IFooterProps> = () => {
         <Typography
           variant="h3"
           fontFamily="Protest Guerrilla"
-          sx={{ marginBottom: '20px' }}
+          sx={{
+            marginBottom: '20px',
+            color:
+              theme.palette.mode === darkModeKey
+                ? colors[theme.palette.mode].black[1000]
+                : colors[theme.palette.mode].black[100],
+          }}
         >
           SOLARTE
         </Typography>
         <CopyToClipboardButton
           label={config.mail}
           textProperties={{
-            color: colors[theme.palette.mode].black[300],
+            color:
+              theme.palette.mode === darkModeKey
+                ? colors[theme.palette.mode].black[1000]
+                : colors[theme.palette.mode].black[300],
             sx: {
               cursor: 'pointer',
             },
           }}
           textToCopy={config.mail}
         />
-        <Typography color={colors[theme.palette.mode].black[300]}>
+        <Typography
+          color={
+            theme.palette.mode === darkModeKey
+              ? colors[theme.palette.mode].black[1000]
+              : colors[theme.palette.mode].black[300]
+          }
+        >
           {config.phoneNumber}
         </Typography>
       </Box>
