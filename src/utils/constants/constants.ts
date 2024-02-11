c96954c07eac0a9e92b94d7c737f4367125d81c0
jason-ptm <js.solarte29@gmail.com>
2024-02-11 13:56:42 -0500

    feat: deploy url

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,4 +1,5 @@
-export const SITE_URL = 'http://localhost:5173/';
+export const SITE_URL =
+  'https://65c91823e532464f88d44d66--solarte-portfolio.netlify.app/';
 export const ENGLISH_CV_ROUTE = `${SITE_URL}/files/Jason Solarte EN CV.pdf`;
 export const SPANISH_CV_ROUTE = `${SITE_URL}/files/Jason Solarte ES CV.pdf`;
 export const CV_NAME = 'Jason Solarte CV.pdf';
