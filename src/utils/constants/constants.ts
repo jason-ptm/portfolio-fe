41179cf626405cefdd6ef75e101ddf99da0acdaf
jason-ptm <js.solarte29@gmail.com>
2024-02-11 13:58:09 -0500

    fix: wrong url

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,5 +1,4 @@
-export const SITE_URL =
-  'https://65c91823e532464f88d44d66--solarte-portfolio.netlify.app/';
+export const SITE_URL = 'https://solarte-portfolio.netlify.app/';
 export const ENGLISH_CV_ROUTE = `${SITE_URL}/files/Jason Solarte EN CV.pdf`;
 export const SPANISH_CV_ROUTE = `${SITE_URL}/files/Jason Solarte ES CV.pdf`;
 export const CV_NAME = 'Jason Solarte CV.pdf';
